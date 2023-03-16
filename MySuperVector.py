10ddf69e36e00aed866887b92257e270b9ccccf4
vestuyy <salauat050307@gmail.com>
2023-03-16 21:29:23 +0600

    Hello my dear frined, i have changed this file before

--- MySuperVector.py
+++ MySuperVector.py
@@ -1,17 +1,2 @@
-class SuperVector:
-    def __init__(self, x, y, x):
-        self.x = 5
-        self.y = 2
-        self.z = 7
-    
-    def add(self, other):
-        return SuperVector(self.x + other.x, self.y + other.y, self.z + other.z)
-    
-    def subtract(self, other):
-        return SuperVector(self.x - other.x, self.y - other.y, self.z - other.z)
-    
-    def multiply(self, scalar):
-        return SuperVector(self.x * scalar, self.y * scalar, self.z * scalar)
-    
-    def __str__(self):
-        return f"({self.x}, {self.y}, {self.z})"
+if __name__ == "__main__":
+    print("hi")
